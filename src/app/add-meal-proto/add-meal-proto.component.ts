60cbfbed1a60101cf2b44371bee9b0b43ee2180d
Hituh <okazysssie@gmail.com>
2022-11-26 23:26:08 +0100

    Button to hide form + finished validation

--- src/app/add-meal-proto/add-meal-proto.component.ts
+++ src/app/add-meal-proto/add-meal-proto.component.ts
@@ -19,7 +19,8 @@ import { MealServiceService } from '../meal-service.service';
 export class AddMealProtoComponent implements OnInit {
   @Output() newMeal = new EventEmitter<MealProtoClass>();
   meals: MealProtoClass[] = [];
-
+  isShowInfo = false;
+  showButton = true;
   formModel: FormGroup;
   ingredients: FormArray;
   ingredientsV2: Array<String> = [];
@@ -36,11 +37,22 @@ export class AddMealProtoComponent implements OnInit {
     );
   }
 
-  findFreeIndex(): void {
-    for (let i = 0; i < this.meals.length; i++)
-      console.log(this.meals[i].CategoryId);
+  findFreeIndex(): number {
+    let indexes: number[] = [];
+    for (let i = 0; i < this.meals.length; i++) {
+      indexes[this.meals[i].Index_nr] = 1;
+    }
+    for (let i = 0; i < indexes.length; i++) {
+      if (indexes[i] == null) {
+        return i;
+      }
+      if (i == this.meals.length - 1) {
+        return i + 1;
+      }
+    }
+    return 0;
   }
-  
+
   getInputIngredient(): Array<String> {
     const ingredientsArrayValues: Array<String> = [];
 
@@ -58,16 +70,23 @@ export class AddMealProtoComponent implements OnInit {
         Validators.minLength(3),
         Validators.maxLength(150),
       ]),
-      timePrep: new FormControl('', [Validators.required]),
-      index_nr: new FormControl(),
-      rating: new FormControl(),
+      timePrep: new FormControl('', [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(999),
+      ]),
+      rating: new FormControl('', [
+        Validators.required,
+        Validators.min(1),
+        Validators.max(5),
+      ]),
       categoryId: new FormControl(),
       ingredientsArray: new FormArray([
         new FormGroup({
-          ingredient: new FormControl(),
+          ingredient: new FormControl('', [Validators.required]),
         }),
       ]),
-      description: new FormControl(),
+      description: new FormControl('', [Validators.required]),
     });
     this.ingredients = this.formModel.get('ingredientsArray') as FormArray;
   }
@@ -75,9 +94,20 @@ export class AddMealProtoComponent implements OnInit {
   get ingredientsArray(): FormArray {
     return this.formModel.get('ingredientsArray') as FormArray;
   }
-
   get name() {
     return this.formModel.get('name');
+  }
+  get timePrep() {
+    return this.formModel.get('timePrep');
+  }
+  get rating() {
+    return this.formModel.get('rating');
+  }
+  get description() {
+    return this.formModel.get('description');
+  }
+  get ingredient() {
+    return this.formModel.get('ingredient');
   }
 
   onAddMeal() {
@@ -86,7 +116,7 @@ export class AddMealProtoComponent implements OnInit {
       new MealProtoClass(
         this.formModel.value.name,
         this.formModel.value.timePrep,
-        this.formModel.value.index_nr,
+        this.findFreeIndex(),
         this.formModel.value.rating,
         this.getInputIngredient(),
         this.formModel.value.description,
